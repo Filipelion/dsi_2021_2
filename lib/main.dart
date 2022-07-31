29f3a1a179d54ea9d9d6799df10d490b5cfd5109
filipelion <14293424+Filipelion@users.noreply.github.com>
2022-07-31 07:34:56 -0300

    Ajuste no layout cardview

--- lib/main.dart
+++ lib/main.dart
@@ -112,30 +112,14 @@ class _RandomWordsState extends State<RandomWords> {
     } else {
       return ListView.builder(
           padding: const EdgeInsets.all(16),
-          // The itemBuilder callback is called once per suggested
-          // word pairing, and places each suggestion into a ListTile
-          // row. For even rows, the function adds a ListTile row for
-          // the word pairing. For odd rows, the function adds a
-          // Divider widget to visually separate the entries. Note that
-          // the divider may be difficult to see on smaller devices.
           itemBuilder: (BuildContext _context, int i) {
-            // Add a one-pixel-high divider widget before each row
-            // in the ListView.
             if (i.isOdd) {
               return Divider();
             }
 
-            // The syntax "i ~/ 2" divides i by 2 and returns an
-            // integer result.
-            // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
-            // This calculates the actual number of word pairings
-            // in the ListView,minus the divider widgets.
             final int index = i ~/ 2;
-            // If you've reached the end of the available word
-            // pairings...
+
             if (index >= _suggestions.length) {
-              // ...then generate 10 more and add them to the
-              // suggestions list.
               _suggestions.addAll(generateWordPairs().take(10));
             }
             return _buildRow(_suggestions[index]);
@@ -147,36 +131,35 @@ class _RandomWordsState extends State<RandomWords> {
     final alreadySaved = _saved.contains(pair);
     if (cardView) {
       return Card(
-        child: GridTile(
-          child: Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  pair.asPascalCase,
-                  style: _biggestFont,
-                ),
-                IconButton(
-                  icon: Icon(
-                    alreadySaved ? Icons.favorite : Icons.favorite_border,
-                    semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-                  ),
-                  color: alreadySaved ? Colors.red : null,
-                  onPressed: () {
-                    setState(
-                      () {
-                        if (alreadySaved) {
-                          _saved.remove(pair);
-                        } else {
-                          _saved.add(pair);
-                        }
-                      },
-                    );
-                  },
-                ),
-              ],
+        clipBehavior: Clip.antiAlias,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              // Todo: recuperar Pair
+              pair.asPascalCase,
+              style: _biggerFont,
             ),
-          ),
+            IconButton(
+              icon: Icon(
+                // Todo: recuperar alreadySaved
+                alreadySaved ? Icons.favorite : Icons.favorite_border,
+                semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+              ),
+              color: alreadySaved ? Colors.red : null,
+              onPressed: () {
+                setState(
+                  () {
+                    if (alreadySaved) {
+                      _saved.remove(pair);
+                    } else {
+                      _saved.add(pair);
+                    }
+                  },
+                );
+              },
+            ),
+          ],
         ),
       );
     } else {
