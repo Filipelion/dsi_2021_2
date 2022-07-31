d606b1b386f59da62dde3495c28a647405d6f63d
filipelion <14293424+Filipelion@users.noreply.github.com>
2022-07-31 06:45:19 -0300

    Implementando segunda parte

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black12,
+          foregroundColor: Colors.black,
+        ),
+      ),
       home: RandomWords(),
     );
   }
@@ -20,40 +26,187 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
+  final _biggestFont = const TextStyle(fontSize: 27);
+  bool cardView = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.crop_square_rounded),
+            onPressed: _changeViwer,
+            tooltip: 'Change View',
+          ),
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          ),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
 
+  void _changeViwer() {
+    setState(() {
+      if (cardView) {
+        cardView = false;
+      } else {
+        cardView = true;
+      }
+    });
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
   Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return Divider();
-          }
+    if (cardView) {
+      return GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            mainAxisSpacing: 20,
+            crossAxisCount: 2,
+            childAspectRatio: 1.3,
+          ),
+          itemBuilder: (BuildContext _context, int i) {
+            final int index = i;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            return _buildRow(_suggestions[index]);
+          });
+    } else {
+      return ListView.builder(
+          padding: const EdgeInsets.all(16),
+          // The itemBuilder callback is called once per suggested
+          // word pairing, and places each suggestion into a ListTile
+          // row. For even rows, the function adds a ListTile row for
+          // the word pairing. For odd rows, the function adds a
+          // Divider widget to visually separate the entries. Note that
+          // the divider may be difficult to see on smaller devices.
+          itemBuilder: (BuildContext _context, int i) {
+            // Add a one-pixel-high divider widget before each row
+            // in the ListView.
+            if (i.isOdd) {
+              return Divider();
+            }
 
-          final int index = i ~/ 2;
-
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
+            // The syntax "i ~/ 2" divides i by 2 and returns an
+            // integer result.
+            // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+            // This calculates the actual number of word pairings
+            // in the ListView,minus the divider widgets.
+            final int index = i ~/ 2;
+            // If you've reached the end of the available word
+            // pairings...
+            if (index >= _suggestions.length) {
+              // ...then generate 10 more and add them to the
+              // suggestions list.
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            return _buildRow(_suggestions[index]);
+          });
+    }
   }
 
   Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
+    final alreadySaved = _saved.contains(pair);
+    if (cardView) {
+      return Card(
+        child: GridTile(
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  pair.asPascalCase,
+                  style: _biggestFont,
+                ),
+                IconButton(
+                  icon: Icon(
+                    alreadySaved ? Icons.favorite : Icons.favorite_border,
+                    semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+                  ),
+                  color: alreadySaved ? Colors.red : null,
+                  onPressed: () {
+                    setState(
+                      () {
+                        if (alreadySaved) {
+                          _saved.remove(pair);
+                        } else {
+                          _saved.add(pair);
+                        }
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    } else {
+      return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+          ),
+          color: alreadySaved ? Colors.red : null,
+          onPressed: () {
+            setState(
+              () {
+                if (alreadySaved) {
+                  _saved.remove(pair);
+                } else {
+                  _saved.add(pair);
+                }
+              },
+            );
+          },
+        ),
+        onTap: () {
+          // NEW lines from here...
+        },
+      );
+    }
   }
 }
